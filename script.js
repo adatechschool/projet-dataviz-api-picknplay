d6ddc6e248e38fe9fe05e84c059a739c2955502c
Jude Abraham <jude.abraham@epitech.eu>
2024-11-28 16:36:01 +0100

    [ADD] new file for home page

--- script.js
+++ script.js
@@ -25,19 +25,4 @@ document.getElementById('refresh-button').addEventListener('click', async () => 
     }
 });
 
-//bouton Home
-const gameSection = document.getElementById('gameContainer')
-const homeButton = document.getElementById('home-button')
-document.body.appendChild(homeButton)
-
-if (localStorage.getItem('isGameSectionHidden') === 'true') {
-    gameSection.style.display = 'none';
-  }
-
-  homeButton.addEventListener('click', () =>{
-    setTimeout(()=> location.reload(),100)
-    gameSection.style.display = 'none'
-});
-localStorage.setItem('isGameSectionHidden', 'true');
-
 fetchGenres();
