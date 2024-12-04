dc842053fa5504866722768d7826c9ffba97f2c4
RoddickMhg <mahoungouroddick@gmail.com>
2024-12-04 12:00:00 +0100

    home page V2

--- home_page.js
+++ home_page.js
@@ -1,4 +1,5 @@
-//bouton Home
+
+const sideBar = document.getElementById('side-bar')
 const getGame = document.getElementById('get-game')
 const homeIcon  = document.getElementById('home-icon')
 const refreshBtn = document.getElementById('refresh-button')
@@ -6,48 +7,56 @@ const stores = document.getElementById('stores')
 const Home = document.getElementById('home')
 const gameSection = document.getElementById('gameContainer')
 const homeButton = document.getElementById('home-button')
-document.body.appendChild(homeButton)
+
+
+function displayNone(){
+    gameSection.style.display = 'none'
+    refreshBtn.style.display = 'none'
+    Home.style.display = 'none'
+    stores.style.display = 'none'
+    homeIcon.style.display = 'none'
+    refresh.style.display = 'none'
+    sideBar.style.display = 'none'
+}
+
+function displayBlock(){
+    refreshBtn.style.display = 'block'
+        Home.style.display = 'block'
+        stores.style.display = 'block'
+        homeIcon.style.display = 'block'
+        refresh.style.display = 'block'
+        sideBar.style.display = 'block'
+}
+
 
 if (localStorage.getItem('isGameSectionHidden') === 'true') {
-        gameSection.style.display = 'none'
-        refreshBtn.style.display = 'none'
-        Home.style.display = 'none'
-        stores.style.display = 'none'
-        homeIcon.style.display = 'none'
-        refresh.style.display = 'none'
+        displayNone()
   }
 
+    
     homeButton.addEventListener('click', () =>{
     setTimeout(()=> location.reload(),100)
     gameSection.style.display = 'none'
-    localStorage.setItem('isGameSectionHidden', 'true')
+    localStorage.setItem('isGameSectionHidden', 'true') 
+        
+        displayNone()
 
-        refreshBtn.style.display = 'none'
-        Home.style.display = 'none'
-        stores.style.display = 'none'
-        homeIcon.style.display = 'none'
-        refresh.style.display = 'none'
 });
+
 
 getGame.addEventListener('click', () =>{
     if(gameSection.style.display === 'none'){
         gameSection.style.display = 'block'
         localStorage.setItem('isGameSectionHidden' ,'false' )
 
-        refreshBtn.style.display = 'block'
-        Home.style.display = 'block'
-        stores.style.display = 'block'
-        homeIcon.style.display = 'block'
-        refresh.style.display = 'block'
-    }else{
+         displayBlock()
+
+    } else {
         gameSection.style.display = 'none'
         localStorage.setItem('isGameSectionHidden' ,'true' )
-
-        refreshBtn.style.display = 'none'
-        Home.style.display = 'none'
-        stores.style.display = 'none'
-        homeIcon.style.display = 'none'
-        refresh.style.display = 'none'
+            
+        displayNone()
 
     }
 } )
+ 
